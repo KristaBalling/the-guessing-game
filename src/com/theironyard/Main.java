fa8835fb2b0aee26493e7a68334c679e613cb433
Krista Blachian <KristaBlachian@outlook.com>
2018-01-12 23:42:49 -0500

    completed game

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GuessGame game = new com.theironyard.GuessGame();
+        game.startGame();
+    }
+}
